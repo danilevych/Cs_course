3892019dde999c128db7db11892388fe7978f901
Julia Danilevych <danilevych.julia@gmail.com>
2023-09-22 20:25:19 +0300

    Added HomeWork1

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Transactions;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter a value (side a square):");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int squareArea = a * a;
+            int squarePerimeter = 2 * (a + a);
+
+            Console.WriteLine($"Results:\n Side of area {a},\n square area is: {squareArea},\n square perimeter is: {squarePerimeter}");
+
+
+            Console.WriteLine("___________________________");
+
+
+            Console.WriteLine("What is your name?");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"How old are you, {name}?");
+            string? age = Console.ReadLine();
+            Console.WriteLine($"Your name is: {name}\n" +
+                $"Your age is: {age}");
+
+
+            Console.WriteLine("___________________________");
+
+
+            Console.WriteLine("Enter a radius of a circle in a double format:");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double pi = Math.PI;
+            double length = 2 * pi * r;
+            double area = pi * Math.Pow(r, r);
+            //double volume = 4/3 * pi * r * r * r;
+            double volume = 4 / 3 * pi * Math.Pow(r, 3);
+
+            Console.WriteLine($"Results:\n" +
+                $"Radius: {r}\n" +
+                $"Area: {area:F4}\n" +
+                $"Volume: {volume:F4}");
+
+
+            Console.WriteLine("___________________________");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ HomeWork1/README.md
@@ -0,0 +1,17 @@
+# HOMEWORK 1
+
+### _Create Console Application project in VS._
+
+In method Main() write code for solving next tasks:
+
+1. Define integer variable a (side of the square).
+Read value from the console and calculate the area and perimeter of a square with
+length a. Output results on the console.
+
+2. Define string name and integer age.
+Output question "What is your name?";
+Read value name and output next question: "How old are you, (name)?".
+Read value age and output whole information
+
+3. Read double number r (radius of a circle).
+Calculate the length (l=2*pi*r), area (S=pi*r*r), and volume (4/3*pi*r*r*r) of a circle.
