f534424f4da4dd8a0e1ff7312cb0a513df04b365
Julia Danilevych <danilevych.julia@gmail.com>
2023-10-04 19:53:54 +0300

    added HomeWork4

---
+++ HomeWork4/HomeWork4/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    internal class Person
+    {
+        private string name;
+        private DateTime birthTime;
+
+        public string Name { get { return name; } }
+        public DateTime BirthTime { get {  return birthTime; } }
+
+        public Person() 
+        {
+            name = "Facundo";
+            birthTime = new DateTime(2000, 10, 10, 00, 00, 00);
+        }
+
+        public Person(string name, DateTime birthTime)
+        {
+            this.name = name;
+            this.birthTime = birthTime;
+        }
+
+        public int Age()
+        {
+             return DateTime.Now.Year - this.BirthTime.Year;
+            
+        }
+
+        public static Person Input()
+        {
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter birthday date:");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine($"You create: {name}, who was born {birthDate}");
+
+            return new Person(name, birthDate);
+        }
+
+        public void ChangeName(string newName)
+        {
+            this.name = newName;
+            Console.WriteLine($"New name of person: {this.name}");
+        }
+        public override string ToString()
+        {
+            return "Name of Person " + name + "; birth day of person " + birthTime;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public static bool operator == (Person man, Person woman)
+        {
+            return man.Name == woman.Name;
+        }
+        public static bool operator != (Person man, Person woman)
+        {
+            return !(man == woman);
+        }
+
+        
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net.Cache;
+using System.Runtime.CompilerServices;
+
+namespace HomeWork4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int countOfPersons = 6;
+            const int veryYoung = 16;
+
+
+            Person man = new Person();
+            Person woman = new Person("Liza", new DateTime(2024, 08, 08));
+            Console.WriteLine(man.Name);
+            Console.WriteLine(man.BirthTime);
+
+
+            man.Age();
+            woman.Age();
+
+            Person newPerson = Person.Input();
+            Console.WriteLine(newPerson.Name);
+            int age = newPerson.Age();
+            newPerson.ChangeName("Bob");
+
+            Console.WriteLine(man.BirthTime.ToLongDateString());
+
+            Console.WriteLine(man.ToString());
+
+            if (man == woman)
+            {
+                Console.WriteLine("The same name");
+            }
+            else
+            {
+                Console.WriteLine("Not same names");
+            }
+
+            man.Output();
+            woman.Output();
+
+            Console.WriteLine("________________________________________________________");
+
+            Person[] people = new Person[countOfPersons];
+
+
+            for (int i = 0; i < countOfPersons; i++)
+            {
+                Console.WriteLine(($"Enter a name of {i + 1} person"));
+                string name = Console.ReadLine();
+
+                Console.WriteLine(($"Enter a birth day of {i + 1} person"));
+                DateTime birthDay = Convert.ToDateTime(Console.ReadLine());
+
+                people[i] = new Person(name, birthDay);
+                Console.WriteLine(people[i].Age());
+
+
+                people[i].Output();
+                Console.WriteLine("____");
+
+                if (people[i].Age() <= veryYoung)
+                {
+                    Console.WriteLine($"Enter a new name of person {i + 1}:");
+                    people[i].ChangeName("Very Young");
+                }
+                Console.WriteLine("________________________________________________________");
+            }
+
+            for (int i = 0; i < countOfPersons; i++)
+            {
+                people[i].Output();
+            }
+            Console.WriteLine("__________");
+
+            for (int i = 0; i < countOfPersons - 1; i++)
+            {
+                for (int j = i + 1; j < countOfPersons; j++)
+                {
+                    if (people[i] == people[j])
+                    {
+                        Console.WriteLine($"{people[i].ToString()} have the same name with: {people[j].ToString()}");
+                        Console.WriteLine("________________________");
+                    }
+                }
+            }
+            Console.WriteLine("________________________________________________________");
+        }
+    }
+}
