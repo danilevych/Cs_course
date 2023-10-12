84947392d0da228358c6e653f5756c9f60fe16a6
Julia Danilevych <danilevych.julia@gmail.com>
2023-10-12 13:23:31 +0300

    rename class Exception

--- HomeWork6/HomeWork6/obj/Debug/net8.0/HomeWork6.AssemblyInfo.cs
+++ HomeWork6/HomeWork6/obj/Debug/net8.0/HomeWork6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5807393acda8703f5873f021e0b0f03fe1e95c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWork6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWork6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
